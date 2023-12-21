1c67fe9556585a5af6d62e2494e833b6d0dccec8
RenatoEsposito1999 <renato.esposito1999@outlook.com>
2023-12-22 00:52:38 +0100

    Added cpu e gpu conversion from RGB to GRAYSCALE

--- image.cu
+++ image.cu
@@ -2,15 +2,35 @@
 #include <opencv2/core/cuda.hpp>
 #include <opencv2/cudaimgproc.hpp>
 #include <opencv2/cudaarithm.hpp>
+/*N.B: non posso comparare un metodo di libreria con un metodo eseguito a mano quindi o comparo due metodi di libreria o scrivo sia sequenziale che kernel per
+comparare.*/
 
+/*Al momento ho fatto la prima operazione di preprocessing cioè la trasformazione in scala di grigio e l'ho fatta usando l'operazione sia su CPU che GPU,
+in entrambi i casi ho usato la libreari quindi sono comparabili.*/
+
+/*Prossimo step: prendere i tempi e stamparli*/
+cv::Mat cpu_RGBtoGRAYSCALE(cv::Mat);
+cv::Mat gpu_RGBtoGRAYSCALE(cv::Mat);
 
 int main() {
-    cv::Mat input = cv::imread("foto.jpg", cv::IMREAD_ANYCOLOR);
+    cv::Mat cpu_outputGrayScale;
+    cv::Mat gpu_outputGrayScale;
+    cv::Mat input = cv::imread("foto.jpg");
 
     if (input.empty()) {
         fprintf(stderr, "Impossibile caricare l'immagine\n");
         return -1;
-        }
+    }
+    
+    cpu_outputGrayScale = cpu_RGBtoGRAYSCALE(input);
+    
+    cv::imwrite("GrayscaleImageCPU.jpg",cpu_outputGrayScale);
+    
+    gpu_outputGrayScale = gpu_RGBtoGRAYSCALE(input);
+    
+    cv::imwrite("GrayscaleImageGPU.jpg",gpu_outputGrayScale);
+
+    /*
     // Trasferisci l'immagine sulla memoria GPU
     cv::cuda::GpuMat gpuInput;
     cv::cuda::GpuMat gpuGrayImage;
@@ -32,6 +52,22 @@ int main() {
     //cv::imshow("Output.jpg", output);
     //printf("Output\n");
     //cv::waitKey(0);
-
+    */
     return 0;
+}
+
+cv::Mat cpu_RGBtoGRAYSCALE(cv::Mat in){
+    cv::Mat out;
+    cv::cvtColor(in,out,cv::COLOR_BGR2GRAY);
+    return out;
+}
+
+cv::Mat gpu_RGBtoGRAYSCALE(cv::Mat in){
+    cv::Mat out;
+    cv::cuda::GpuMat gray;
+    cv::cuda::GpuMat gpuImage(in);
+    cv::cuda::cvtColor(gpuImage,gray,cv::COLOR_BGR2GRAY);
+    gray.download(out);
+
+    return out;
 }
