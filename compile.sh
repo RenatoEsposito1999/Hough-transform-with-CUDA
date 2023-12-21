cbf07065700142c847ed4fd9f189a1773d3fe982
RenatoEsposito1999 <renato.esposito1999@outlook.com>
2023-12-22 00:56:09 +0100

    Added translation

--- compile.sh
+++ compile.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# Verifica il numero corretto di argomenti
+# Check the correct number of arguments
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 input_file.cu output_executable"
     exit 1
 fi
 
-# Assegna gli argomenti a variabili
+# Assign arguments to variables
 input_file="$1"
 output_executable="$2"
 
-# Verifica che il file di input .cu esista
+# Verify that the .cu input file exists
 if [ ! -f "$input_file" ]; then
-    echo "Il file $input_file non esiste."
+    echo "The file $input_file doesn't exist."
     exit 1
 fi
 
-# Esegui il comando nvcc per compilare il file .cu e cattura gli errori
+# Run the nvcc command to compile the .cu file and catch errors
 if errors=$(nvcc "$input_file" -o "$output_executable" -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_cudaimgproc -lopencv_cudawarping -lopencv_cudaarithm -lopencv_cudafilters -lopencv_cudafeatures2d -lopencv_cudaobjdetect -lopencv_cudabgsegm -lopencv_cudastereo -lopencv_cudev -lcudart -lnppicom -lnppidei -lnppif -lnppig -lnppim -lnppist -lnppisu -lnppitc -lnpps -lcufft -lnvrtc -lnvcuvid -lcudart -lpthread -lm -ldl 2>&1); then
-    echo "Compilazione completata con successo. Eseguibile creato: $output_executable"
-    echo "Esecuzione in corso..."
+    echo "Compilation completed successfully. Executable created: $output_executable"
+    echo "Running..."
     ./"$output_executable"
 else
-    echo "Errore durante la compilazione:"
+    echo "Error during compilation:"
     echo "$errors"
 fi
